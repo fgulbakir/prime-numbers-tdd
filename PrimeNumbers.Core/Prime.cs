95f0296251af6a1d8bc31a4ccd9944d34d741879
fgulbakir <fatmagulbakir61@gmail.com>
2019-12-15 23:40:59 +0300

    prime number with tdd

---
+++ PrimeNumbers.Core/Prime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers.Core
+{
+    public class Prime
+    {
+
+        //public static bool IsPrimeNumber(int number)
+        //{
+        //    return true;
+        //}
+
+        //public static bool IsPrimeNumber(int number)
+        //{
+        //    return number != 4;
+        //}
+
+        //public static bool IsPrimeNumber(int number)
+        //{
+        //    return !(number == 4 || number == 9);
+        //}
+
+        public static bool IsPrimeNumber(int number)
+        {
+            for (var i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+
+
+    }
+}
---
+++ PrimeNumbers.Test/PrimeNumberTest.cs
@@ -0,0 +1,51 @@
+﻿using PrimeNumbers.Core;
+using Xunit;
+
+namespace PrimeNumbers.Test
+{
+    public class PrimeNumberTest
+    {
+        private int[] _primeNumbers;
+
+        public PrimeNumberTest()
+        {
+            _primeNumbers = new[] { 1, 2, 5, 23, 13,41 };
+        }
+
+        [Fact]
+        public void OneIsPrimeNumber()
+        {
+            Assert.True(Prime.IsPrimeNumber(1));
+
+        }
+
+        [Fact]
+        public void FourIsNotPrimeNumber()
+        {
+            Assert.False(Prime.IsPrimeNumber(4));
+
+        }
+
+
+        [Fact]
+        public void NineIsNotPrimeNumber()
+        {
+            Assert.False(Prime.IsPrimeNumber(9));
+
+        }
+
+
+
+        [Fact]
+        public void IsPrimeNumber()
+        {
+            foreach (var primeNumber in _primeNumbers)
+            {
+                Assert.True(Prime.IsPrimeNumber(primeNumber));
+            }
+
+        }
+
+
+    }
+}
